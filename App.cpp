fcd8094c499e5d6f9d4695830a0b391162fb3ffb
NOAHBENJ <noahbenj6@gmail.com>
2024-11-03 20:52:40 +1100

    broken commit for copilot to see

--- App.cpp
+++ App.cpp
@@ -2,10 +2,13 @@
 #include "MainFrame.h"
 #include <wx/wx.h>
 #include <wx/settings.h>
+#include <wx/image.h> 
 
 wxIMPLEMENT_APP(App);
 
 bool App::OnInit() {
+	wxInitAllImageHandlers();
+	wxImage::AddHandler(new wxJPEGHandler());
 	MainFrame* mainFrame = new MainFrame("RayLauncher");
 	mainFrame->SetClientSize(800, 600);
 	mainFrame->Center();
