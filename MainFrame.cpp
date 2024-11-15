89584718b4f230b017e646309e4c90d60103a914
NOAHBENJ <noahbenj6@gmail.com>
2024-11-15 19:56:46 +1100

    Removed unused exemplar class

--- MainFrame.cpp
+++ MainFrame.cpp
@@ -22,32 +22,6 @@ libcurl.dll MSVCP140D.dll VCRUNTIME140D.dll VCRUNTIME140_1D.dll
 // DLL's that dont work
 */
 
-// TEST
-class ScrolledWidgetsPane : public wxScrolledWindow
-{
-public:
-    ScrolledWidgetsPane(wxWindow* parent, wxWindowID id) : wxScrolledWindow(parent, id)
-    {
-        // the sizer will take care of determining the needed scroll size
-        // (if you don't use sizers you will need to manually set the viewport size)
-        wxBoxSizer* sizer = new wxBoxSizer(wxVERTICAL);
-
-        // add a series of widgets
-        for (int w = 1; w <= 120; w++)
-        {
-            wxButton* b = new wxButton(this, wxID_ANY, wxString::Format(wxT("Button %i"), w));
-            sizer->Add(b, 0, wxALL, 3);
-        }
-
-        this->SetSizer(sizer);
-
-        // this part makes the scrollbars show up
-        this->FitInside(); // ask the sizer about the needed size
-        this->SetScrollRate(5, 5);
-    }
-
-};
-
 using json = nlohmann::json;
 
 const wxColour darkPurple = wxColour(113, 96, 232, 100);
